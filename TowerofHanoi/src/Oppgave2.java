c4310ccb2b6d0ea31683090effa7beed3fc3ceda
IamJohandy <540195@student.hin.no>
2015-03-03 20:15:06 +0100

    versjon 0.2

--- TowerofHanoi/src/Oppgave2.java
+++ TowerofHanoi/src/Oppgave2.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Oppgave2 {
+	static int antallForflyttninger = 0;
+	static int antallKall = 0;
+	
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
@@ -9,14 +12,18 @@ public class Oppgave2 {
 		
 		System.out.println("Følgende forflyttninger er gjort: ");
 		flyttDisk(n, 'A', 'B', 'C');
+		System.out.println("Antall forflyttninger: " + antallForflyttninger);
 	}
 	
 	public static void flyttDisk(int n, char fraTaarn, char tilTaarn, char auxTaarn) {
-		if(n==1)
+		if(n==1) {
 			System.out.println("Flytt disk " + n + " fra " + fraTaarn + " til " + tilTaarn);
+			antallForflyttninger++;
+		}
 		else{
 			flyttDisk(n - 1, fraTaarn, auxTaarn, tilTaarn);
 			System.out.println("Flytt disk " + n + " fra " + fraTaarn + " til " + tilTaarn);
+			antallForflyttninger++;
 			flyttDisk(n - 1, auxTaarn, tilTaarn, fraTaarn);
 		}
 	}
