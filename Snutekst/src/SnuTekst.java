81848f2b56fab328d87a90f89706ea9a13f23b86
IamJohandy <540195@student.hin.no>
2015-03-03 19:27:28 +0100

    versjon 0.2

--- Snutekst/src/SnuTekst.java
+++ Snutekst/src/SnuTekst.java
@@ -1,21 +1,27 @@
 import java.util.Scanner;
 
 public class SnuTekst {
+	static int antallTegn = 0;
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Skriv inn ønsket tekst du vil snu: ");
 		String str = input.nextLine();
 		System.out.print("Din string baklengs er: ");
-		baklengs(str);	
+		baklengs(str);
+		System.out.println("Antall tegn som ble snudd: " + antallTegn);
 	}
 	
 	public static void baklengs(String tekst) {
 		int lengde = tekst.length();
-		if(lengde == 1)
+		if(lengde == 1){
 			System.out.println(tekst.charAt(0));
+			antallTegn++;
+		}
 		else {
 			System.out.print(tekst.charAt(lengde - 1));
 			tekst = tekst.substring(0, tekst.length()-1);
+			antallTegn++;
 			baklengs(tekst);
 		}
 	}
